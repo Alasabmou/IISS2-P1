ca54268bc6cd9887f42862e9890dba98da9ad4b2
Alasabmou <alasabmou@alum.us.es>
2026-02-24 17:50:39 +0100

    fix: update package.json to include dependencies for package-lock.json and package.json

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,8 +6,84 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      postalCode: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      shippingCosts: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+      },
+      averageServiceMinutes: {
+        type: Sequelize.DOUBLE,
+        allowNull: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      logo: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      heroImage: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      status: {
+        type: Sequelize.ENUM('online', 'offline', 'closed', 'temporarily closed'),
+        allowNull: false,
+        defaultValue: 'online'
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'RestaurantCategories',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
-      // TODO: Include the rest of the fields of the Restaurants table
     })
   },
   down: async (queryInterface, Sequelize) => {
